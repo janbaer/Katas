9494e71f1b490ccc64c899a683a50434a0ebeb96
Jan Baer <jan@janbaer.de>
2013-01-06 10:28:00 +0100

    Second implementation of the TicTacToe Kata

---
+++ Kata.TicTacToe/Game.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.TicTacToe
+{
+    internal class Game
+    {
+        public const char PLAYER1 = 'o';
+        public const char PLAYER2 = 'x';
+        public const char NOWINNER = '\0';
+
+        private const char EMPTY_CELL = '\0';
+
+        private readonly char[,] _cells = new char[3,3];
+
+        public bool MarkCell(int x, int y, char player)
+        {
+            if (_cells[x, y] == EMPTY_CELL)
+            {
+                _cells[x, y] = player;
+                return true;
+            }
+
+            return false;
+        }
+
+        public char GetWinner()
+        {
+            var players = new List<char>(){Game.PLAYER1, Game.PLAYER2};
+
+            return players.FirstOrDefault(player => CheckRows(player) || CheckColumns(player) || CheckDiagonal(player));
+        }
+
+        private bool CheckDiagonal(char player)
+        {
+            return _cells[0, 0] == player && _cells[1, 1] == player && _cells[2, 2] == player;
+        }
+
+        private bool CheckColumns(char player)
+        {
+            return Enumerable.Range(0, 3).Any(x => Enumerable.Range(0, 3).All(y => _cells[x, y] == player));
+
+        }
+
+        private bool CheckRows(char player)
+        {
+            return Enumerable.Range(0, 3).Any(y => Enumerable.Range(0, 3).All(x => _cells[x, y] == player));
+
+        }
+
+        public bool IsDraw()
+        {
+            if (Enumerable.Range(0, 2).All(x=> Enumerable.Range(0, 2).All(y=> _cells[x, y] != Game.EMPTY_CELL)))
+            {
+                return GetWinner() == Game.NOWINNER;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Kata.TicTacToe/TicTacToeTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Kata.TicTacToe
+{
+    [TestFixture]
+    class TicTacToeTests
+    {
+        [Test]
+        public void MarkCell_when_cell_is_empty_it_should_return_true()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1).Should().BeTrue();
+
+        }
+
+        [Test]
+        public void MarkCell_when_cell_is_not_empty_it_should_return_false()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(0, 0, Game.PLAYER2).Should().BeFalse();
+        }
+
+        [Test]
+        public void GetWinner_when_no_rule_is_succeeded_it_should_return_nowinner()
+        {
+            Game game = new Game();
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(1, 0, Game.PLAYER1);
+
+            game.GetWinner().ShouldBeEquivalentTo(Game.NOWINNER);
+        }
+
+        [Test]
+        public void GetWinner_when_all_cells_in_a_row_are_marked_from_player1_it_should_return_player1()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(1, 0, Game.PLAYER1);
+            game.MarkCell(2, 0, Game.PLAYER1);
+
+            game.GetWinner().ShouldBeEquivalentTo(Game.PLAYER1);
+        }
+        
+        [Test]
+        public void GetWinner_when_all_cells_in_a_row_are_marked_from_player2_it_should_return_player2()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER2);
+            game.MarkCell(1, 0, Game.PLAYER2);
+            game.MarkCell(2, 0, Game.PLAYER2);
+
+            game.GetWinner().ShouldBeEquivalentTo(Game.PLAYER2);
+        }
+
+        [Test]
+        public void GetWinner_when_all_cells_in_a_column_are_marked_from_player1_it_should_return_player1()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(0, 1, Game.PLAYER1);
+            game.MarkCell(0, 2, Game.PLAYER1);
+
+            game.GetWinner().ShouldBeEquivalentTo(Game.PLAYER1);
+        }
+
+        [Test]
+        public void GetWinner_when_all_cells_in_the_diagonal_are_marked_from_player1_it_should_return_player1()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(1, 1, Game.PLAYER1);
+            game.MarkCell(2, 2, Game.PLAYER1);
+
+            game.GetWinner().ShouldBeEquivalentTo(Game.PLAYER1);
+        }
+
+        [Test]
+        public void IsDraw_When_all_cells_are_marked_and_no_rule_is_succeeded_then_it_should_return_true()
+        {
+            Game game = new Game();
+
+            game.MarkCell(0, 0, Game.PLAYER1);
+            game.MarkCell(0, 1, Game.PLAYER2);
+            game.MarkCell(0, 2, Game.PLAYER2);
+            game.MarkCell(1, 0, Game.PLAYER2);
+            game.MarkCell(1, 1, Game.PLAYER1);
+            game.MarkCell(1, 2, Game.PLAYER1);
+            game.MarkCell(2, 0, Game.PLAYER1);
+            game.MarkCell(2, 1, Game.PLAYER2);
+            game.MarkCell(2, 2, Game.PLAYER2);
+
+            game.IsDraw().Should().BeTrue();
+        }
+    }
+}
