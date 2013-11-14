0b4f1f1eebfa059acd6a6ddd4bc916280e9bbe9e
Jan Baer <jan@janbaer.de>
2013-11-14 12:02:10 +0100

    Moved the StringExtensions class to another file

--- Kata.StringCalculator2/StringCalculatorTests.cs
+++ Kata.StringCalculator2/StringCalculatorTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using NUnit.Framework;
 using FluentAssertions;
 
@@ -64,32 +63,5 @@ namespace Kata.StringCalculator
 
 
          
-    }
-
-    public static class StringExtensions
-    {
-        public static int Add(this string input)
-        {
-            const string delimiter = "//[delimiter]\n";
-
-            if (string.IsNullOrEmpty(input))
-            {
-                return 0;
-            }
-
-            if (input.StartsWith(delimiter))
-            {
-                input = input.Substring(delimiter.Length);
-            }
-
-            var numbers = input.Split(new [] {',', '\n'}).Select(int.Parse).Where(n=>n<=1000).ToList();
-
-            if (numbers.Any(n=> n < 0))
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-
-            return numbers.Sum();
-        }
     }
 }
