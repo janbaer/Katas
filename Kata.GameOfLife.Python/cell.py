9713f7ac4775055d7de1074730638f687c5212e0
Jan Baer <jan@janbaer.de>
2015-10-12 08:08:04 +0200

    Change string-format

--- Kata.GameOfLife.Python/cell.py
+++ Kata.GameOfLife.Python/cell.py
@@ -4,7 +4,8 @@ class Cell:
     self.y = y
 
   def __str__(self):
-    return "x: {0}, y: {1}".format(self.x, self.y)
+    #return "x: {0}, y: {1}".format(self.x, self.y)
+    return "x: %d, y: %d" % (self.x, self.y)
 
   def equals(self, otherCell):
     return self.x == otherCell.x and self.y == otherCell.y
