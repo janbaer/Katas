a7aed35716c745f11814c7327b550f5ed7ccbcb1
Jan Baer <jan@janbaer.de>
2015-12-23 17:51:38 +0100

    Update GameOfLife.JavaScript to latest version of kata-generator

--- Kata.GameOfLife.JavaScript/karma.conf.js
+++ Kata.GameOfLife.JavaScript/karma.conf.js
@@ -1,82 +1,33 @@
-// Karma configuration
-// Generated on Wed Sep 18 2013 08:04:02 GMT+0200 (CEST)
-
 module.exports = function (config) {
   'use strict';
 
   config.set({
-
-    // base path, that will be used to resolve files and exclude
     basePath: '',
-
-
-    // frameworks to use
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
-      'test/phantom-polyfill.js',
-      'test/jasmine-aliases.js',
-      'src/**/*.js',
-      'test/**/*.js'
+      'jasmine-aliases.js',
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'src/**/*.js'
     ],
-
-
-    // list of files to exclude
-    exclude: [
-
-    ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    exclude: [],
     reporters: ['spec', 'growl'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
     browsers: ['PhantomJS'],
-
-
-    // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
     singleRun: false,
 
     preprocessors: {
-      'src/**/*.js': ['6to5'],
-      'test/**/*.js': ['6to5']
+      
+      'src/**/*.js': ['babel']
+      
     },
-
-    '6to5Preprocessor': {
+    
+    'babelPreprocessor': {
       options: {
+        presets: ['es2015'],
         sourceMap: 'inline'
       },
       filename: function(file) {
@@ -85,6 +36,9 @@ module.exports = function (config) {
       sourceFileName: function(file) {
         return file.originalPath;
       }
-    }
+    },
+    
+    
+    
   });
 };
