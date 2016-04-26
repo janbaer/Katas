ce5c2369c65f1c644bd3f39b2ab10fae18835a97
Jan Baer <jan@janbaer.de>
2016-04-26 20:44:11 +0200

    Game of live with JS, not finished

--- Kata.GameOfLife.JavaScript/src/gameoflife.js
+++ Kata.GameOfLife.JavaScript/src/gameoflife.js
@@ -1,2 +1,26 @@
 'use strict';
 
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  equals(anotherCell) {
+   return this.x === anotherCell.x &&
+          this.y === anotherCell.y;
+  }
+}
+
+class Game {
+  constructor(...aliveCells) {
+    this.aliveCells = aliveCells;
+  }
+
+  isAlive(cell) {
+    return this.aliveCells.some((aliveCell) => aliveCell.equals(cell));
+  }
+
+  newGeneration() {
+  }
+}
--- Kata.GameOfLife.JavaScript/src/gameoflife.spec.js
+++ Kata.GameOfLife.JavaScript/src/gameoflife.spec.js
@@ -1,9 +1,56 @@
+/* globals Cell, Game */
 'use strict';
 
-describe('Give it some context', function () {
-  describe('maybe a bit more context here', function () {
-    it('should run here few assertions', function () {
-      expect(true).toEqual(true);
+//import { Cell, Game } from 'gameoflife.js';
+
+describe('Game of life spec', function() {
+  describe('isAlive tests', function() {
+    describe('When a cell is alive', function() {
+      beforeEach(function() {
+        this.game = new Game(new Cell(0, 0));
+      });
+
+      it('Should return true', function() {
+        expect(this.game.isAlive(new Cell(0, 0))).toEqual(true);
+      });
     });
+
+    describe('When a cell is not alive', function() {
+      beforeEach(function() {
+        this.game = new Game(new Cell(0, 0));
+      });
+
+      it('Should return false', function() {
+        expect(this.game.isAlive(new Cell(0, 1))).toEqual(false);
+      });
+    });
+  });
+
+
+  describe('NewGeneration tests', function() {
+    describe('When a living cell has 2 alive neighbors', function() {
+      beforeEach(function() {
+        this.game = new Game(new Cell(0, 0), new Cell(0, 1), new Cell(1,1));
+
+        this.game.newGeneration();
+      });
+
+      it('Should stay alive', function() {
+        expect(this.game.isAlive(new Cell(0, 0))).toEqual(true);
+      });
+    });
+
+    describe('When a living cell has less than 2 alive neighbors', function() {
+      beforeEach(function() {
+        this.game = new Game(new Cell(0, 0), new Cell(0, 1));
+
+        this.game.newGeneration();
+      });
+
+      it('Should stay alive', function() {
+        expect(this.game.isAlive(new Cell(0, 0))).toEqual(false);
+      });
+    });
+
   });
 });
